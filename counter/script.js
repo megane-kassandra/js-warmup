22ae428e4be62e320fd2a88a9defc1b5bf68d134
Megane Kassandra <kassandra.matsinkou@facsciences-uy1.com>
2026-09-10 15:50:53 +0100

    premier commit

---
+++ counter/script.js
@@ -0,0 +1,16 @@
+let age=18 ;
+const name="john";
+let isStudent=true;
+
+console.log("The type of isStudent is: " + typeof isStudent)
+console.log("The type of age is: " + typeof age)
+console.log("The type of name is: " + typeof name)
+
+
+console.log("The sum of 5 and 3 is: " + (5 + 3));
+console.log("The multiplication of 5 and 3 is: " + (5 * 3));
+console.log("The division of 5 and 3 is: " + (5 / 3));
+console.log("The subtraction of 5 and 3 is: " + (5 - 3));
+
+console.log("Is equal to 5? " + (5 == "5"));
+console.log("Is equal to 5? " + (5 === "5"));
